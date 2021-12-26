bb713fef02635c4dc25779d3af0652590cf99f91
Hoyeon Jang <46728564+qkeb700@users.noreply.github.com>
2021-12-27 02:24:53 +0900

    feat: Modify a list
    New modify html file sends new data to update form.

--- src/main/java/net/hoyeon/springboard/controller/BoardController.java
+++ src/main/java/net/hoyeon/springboard/controller/BoardController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class BoardController {
@@ -35,4 +37,19 @@ public class BoardController {
         return "view";
     }
 
+    @GetMapping("/modify/{id}")
+    public String spBoardModify(@PathVariable("id") Integer id, Model model){
+        model.addAttribute("spboard", spBoardService.boardView(id));
+        return "modify";
+    }
+
+    @PostMapping("/update/{id}")
+    public String spBoardUpdate(@PathVariable("id") Integer id, SpBoard spBoard, MultipartFile file) throws  Exception{
+        SpBoard spboardTemp = spBoardService.boardView(id);
+        spboardTemp.setTitle(spBoard.getTitle());
+        spboardTemp.setContent(spBoard.getContent());
+
+        spBoardService.write(spboardTemp);
+        return "redirect:/list";
+    }
 }
